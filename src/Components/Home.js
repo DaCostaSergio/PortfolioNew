f5cdd656e93697da692731997c39cda1a2ce6701
DaCostaSergio <47104470+DaCostaSergio@users.noreply.github.com>
2023-01-23 13:29:40 +0100

    change faveicon

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,14 +8,14 @@ import videoIphone from "../Assets/myVideoIPHONE.mp4"
 export default function () {
   return (
     <div className="md:mx-10">
-      <div>
+      <div className="">
         <div classname="">
           <h1 className='pt-24 font-N27  text-center text-xl md:text-5xl lg:text-7xl pt-20 lg:pt-[75px]  z-30 '>
           HI! I'M SERGIO DA COSTA - BASED IN SWITZERLAND
           </h1>
         </div>
-        <div>
-          <div classname=" grid place-content-center">
+        <div >
+          <div classname="mt-[50%]" >
             <video width="100%" height="100%" preload='auto' autoplay="true" muted="true" loop="true" playsinline="true" >
               <source src={video} type="video/mp4" />
               Your browser does not support the video tag.
@@ -24,7 +24,7 @@ export default function () {
         </div>
       </div>
       <div classname="">
-        <h1 className="absolute bottom-0 w-full font-N27 text-center text-xl md:text-5xl lg:text-7xl ">
+        <h1 className=" font-N27 text-center text-xl md:text-5xl lg:text-7xl ">
           <span className='text-center'> GRAPHIC DESIGNER AND WEB DEVELOPER   </span>
         </h1>
       </div>
